085af741169ef81dfa50a86ced62493151be1c52
Ella R <ryukhinaella@gmail.com>
2022-10-08 18:40:34 -0700

    Generate four passwords for one time

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-."
-// let generatePsw = document.getElementById("generatePsw")
+const copied = document.getElementById('copied')
 
-let clickCount = 0
-function generateFirstPassword() {
-    if (clickCount % 2 == 0) {
-        firstPswClicked()
-    } else {
-        secondPswClicked()
+
+function generatePsw() {
+    const passwordEls = document.querySelectorAll('.password')
+    let passwords = []
+    for (let i = 0; i < passwordEls.length; i++) {
+        const password = generatePassword()
+        passwords.push(password)
     }
-    clickCount++
+    for (let i = 0; i < passwords.length; i++) {
+        passwordEls[i].textContent = passwords[i]
+    }
+
+    passwordEls.forEach(el => {
+        el.addEventListener('click', copyPassword)
+    })
+}
+function generatePassword() {
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    const numbers = '1234567890'
+    const logograms = '#$%&@^`~'
+    const mathSymbols = '<*+!?='
+    let characters = uppercase + lowercase + numbers + logograms + mathSymbols;
+    characters = characters.split('')
+    let password = '';
+    for (let i = 0; i < 12; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+    return password
 }
 
 
 
-const firstPswClicked = () => {
-    let passwordOutputOne = document.getElementById("firstGenerated")
-
-    let result = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 13; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength))
-    }
-    passwordOutputOne.textContent = result
-}
-
-const secondPswClicked = () => {
-    let passwordOutputTwo = document.getElementById("secondGenerated")
-    let result = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 13; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength))
-    }
-    passwordOutputTwo.textContent = result
-}
